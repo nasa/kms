fe902a1198b76a75f98808db35f5df2af70b1e78
Christopher Gokey <christopher.d.gokey@nasa.gov>
2025-03-26 13:22:15 -0400

    KMS-522: Fixed eslint issue

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = () => {
     publishStateMachineArn:
       isOffline
         ? 'defaultArnForLocalDev'
+        // eslint-disable-next-line no-template-curly-in-string
         : (process.env.PUBLISH_STATE_MACHINE_ARN || '${cf:${self:service}-${self:provider.stage}.PublishProcessStepFunctionArn}')
   }
 }
