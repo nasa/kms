56c356a568ec39e6ebd013a5d07a5e03bb457947
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2025-06-12 12:21:11 -0400

    KMS-568: Grouped vars and handled null values a bit better.

--- serverless/src/shared/logAnalyticsData.js
+++ serverless/src/shared/logAnalyticsData.js
@@ -7,52 +7,56 @@ export const logAnalyticsData = ({
   search = null
 }) => {
   const {
-    queryStringParameters = {},
-    pathParameters = {},
-    headers: rawHeaders = {},
-    requestContext = {}
+    queryStringParameters,
+    pathParameters,
+    headers: rawHeaders,
+    requestContext
   } = event
 
-  const headers = downcaseKeys(rawHeaders)
+  try {
+    const headers = downcaseKeys(rawHeaders || {})
 
-  const { conceptId, shortName, altLabel } = pathParameters
-  const { scheme, format } = queryStringParameters
-  const version = queryStringParameters?.version
-  const clientIp = headers['x-forwarded-for']
-  const clientId = headers['client-id']
-  const { domainName } = requestContext
-  const { path } = requestContext
-  const protocol = headers['x-forwarded-proto'] || 'https'
-  const action = requestContext.httpMethod
+    const { conceptId, shortName, altLabel } = pathParameters || {}
+    const { scheme, format, version } = queryStringParameters || {}
+    const { domainName, path, httpMethod } = requestContext || {}
 
-  if (!clientIp || !domainName || !path || !protocol || !action) {
-    return
-  }
+    const clientIp = headers['x-forwarded-for']
+    const clientId = headers['client-id']
+    const protocol = headers['x-forwarded-proto'] || 'https'
 
-  const queryString = Object.keys(queryStringParameters).length > 0
-    ? `?${new URLSearchParams(queryStringParameters).toString()}`
-    : ''
-  const fullUrl = `${protocol}://${domainName}${path}${queryString}`
-  const { functionName = '' } = context
-  const userAgent = headers['user-agent']
-
-  let logObject = {
-    analytics: {
-      clientIp,
-      clientId,
-      url: fullUrl,
-      resource: functionName,
-      action,
-      format,
-      version,
-      scheme,
-      concept: conceptId ?? shortName ?? altLabel,
-      search,
-      userAgent
+    if (!clientIp || !domainName || !path || !protocol || !httpMethod) {
+      return
     }
+
+    // Ensure queryStringParameters is an object before using Object.keys
+    const queryString = queryStringParameters && typeof queryStringParameters === 'object' && Object.keys(queryStringParameters).length > 0
+      ? `?${new URLSearchParams(queryStringParameters).toString()}`
+      : ''
+
+    const fullUrl = `${protocol}://${domainName}${path}${queryString}`
+    const { functionName = '' } = context
+    const userAgent = headers['user-agent']
+
+    let logObject = {
+      analytics: {
+        clientIp,
+        clientId,
+        url: fullUrl,
+        resource: functionName,
+        action: httpMethod,
+        format,
+        version,
+        scheme,
+        concept: conceptId ?? shortName ?? altLabel,
+        search,
+        userAgent
+      }
+    }
+
+    logObject = removeEmpty(logObject)
+
+    console.log(JSON.stringify(logObject))
+  } catch (error) {
+    console.error('Error in logAnalyticsData:', error)
   }
-
-  logObject = removeEmpty(logObject)
-
-  console.log(JSON.stringify(logObject))
 }
