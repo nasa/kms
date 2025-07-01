a0d0e9d19d6a47f7734b41b570e1501681dd5f03
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2025-07-01 13:09:20 -0400

    KMS-600: Removed redundant prefixes

--- serverless/src/shared/operations/queries/getProviderUrlsQuery.js
+++ serverless/src/shared/operations/queries/getProviderUrlsQuery.js
@@ -2,8 +2,6 @@ import prefixes from '@/shared/constants/prefixes'
 
 export const getProviderUrlsQuery = (scheme) => `
   ${prefixes}
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX gcmd: <https://gcmd.earthdata.nasa.gov/kms#>
     SELECT ?subject ?bp ?bo
     WHERE {
       ?subject skos:inScheme <https://gcmd.earthdata.nasa.gov/kms/concepts/concept_scheme/${scheme}> .
