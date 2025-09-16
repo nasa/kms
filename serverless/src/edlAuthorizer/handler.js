d21a5c71792336c3e96723765dedb2221d61280a
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2025-09-16 14:16:42 -0400

    KMS-635: Added logging for edl authorizer.

--- serverless/src/edlAuthorizer/handler.js
+++ serverless/src/edlAuthorizer/handler.js
@@ -8,30 +8,43 @@ import { generatePolicy } from '@/shared/generatePolicy'
  * @param {Object} context Methods and properties that provide information about the invocation, function, and execution environment
  */
 export const edlAuthorizer = async (event) => {
-  const {
-    headers = {},
-    methodArn,
-    authorizationToken
-  } = event
+  try {
+    console.log('in edl authorizer')
+    const {
+      headers = {},
+      methodArn,
+      authorizationToken
+    } = event
 
-  // First, try to get the token from headers (case-insensitive)
-  let launchpadToken = downcaseKeys(headers).authorization
+    // First, try to get the token from headers (case-insensitive)
+    let launchpadToken = downcaseKeys(headers).authorization
+    console.log('dbg1 token is ', launchpadToken)
 
-  // If not found in headers, check if it's directly in the event as authorizationToken
-  if (!launchpadToken && authorizationToken) {
-    launchpadToken = authorizationToken
+    // If not found in headers, check if it's directly in the event as authorizationToken
+    if (!launchpadToken && authorizationToken) {
+      launchpadToken = authorizationToken
+    }
+
+    console.log('dbg2 token is ', launchpadToken)
+
+    // If still not found, default to an empty string
+    launchpadToken = launchpadToken || ''
+    const profile = await fetchEdlProfile(launchpadToken)
+    console.log('profile is', profile)
+
+    const { uid } = profile
+
+    if (uid) {
+      console.log('got uid ', uid)
+
+      return generatePolicy(uid, 'Allow', methodArn)
+    }
+
+    throw new Error('Unauthorized')
+  } catch (error) {
+    console.error('Error in EDL authorizer', error)
+    throw new Error('Unauthorized2')
   }
-
-  // If still not found, default to an empty string
-  launchpadToken = launchpadToken || ''
-  const profile = await fetchEdlProfile(launchpadToken)
-  const { uid } = profile
-
-  if (uid) {
-    return generatePolicy(uid, 'Allow', methodArn)
-  }
-
-  throw new Error('Unauthorized')
 }
 
 export default edlAuthorizer
