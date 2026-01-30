fc57de7e09f30d605fc2079a4c1304b1bef7543a
Christopher D. Gokey <christopher.d.gokey@nasa.gov>
2026-01-30 16:30:56 -0500

    KMS-645: Removed defaults

--- bin/deploy-bamboo.sh
+++ bin/deploy-bamboo.sh
@@ -70,9 +70,9 @@ dockerRun() {
         --env "EXISTING_API_ID=$bamboo_EXISTING_API_ID" \
         --env "ROOT_RESOURCE_ID=$bamboo_ROOT_RESOURCE_ID" \
         --env "LOG_LEVEL=$bamboo_LOG_LEVEL" \
-        --env "KMS_CACHE_TTL_SECONDS=${bamboo_KMS_CACHE_TTL_SECONDS:-3600}" \
-        --env "KMS_CACHE_CLUSTER_SIZE_GB=${bamboo_KMS_CACHE_CLUSTER_SIZE_GB:-0.5}" \
-        --env "KMS_CACHE_CLUSTER_ENABLED=${bamboo_KMS_CACHE_CLUSTER_ENABLED:-true}" \
+        --env "KMS_CACHE_TTL_SECONDS=${bamboo_KMS_CACHE_TTL_SECONDS}" \
+        --env "KMS_CACHE_CLUSTER_SIZE_GB=${bamboo_KMS_CACHE_CLUSTER_SIZE_GB}" \
+        --env "KMS_CACHE_CLUSTER_ENABLED=${bamboo_KMS_CACHE_CLUSTER_ENABLED}" \
     $dockerTag "$@"
 }
 
